971e78f12f9137315dfd6c3ea6ed0df8918f21f6
andy <andysalerno@gmail.com>
2018-04-18 21:27:59 -0700

    add missing .rs file

---
+++ src/man_parse/font_style.rs
@@ -0,0 +1,44 @@
+use text_format::text_format::TextFormat;
+
+/// Simple struct holding state
+/// for font styling: bold, italic, underlined
+#[derive(Default)]
+pub struct FontStyleState {
+    bold: bool,
+    italic: bool,
+    underlined: bool,
+}
+
+pub enum FontStyle {
+    Bold,
+    Italic,
+    Underlined,
+}
+
+impl FontStyleState {
+    pub fn set_fontstyle_value(&mut self, s: FontStyle, val: bool) {
+        match s {
+            FontStyle::Bold => self.bold = val,
+            FontStyle::Italic => self.italic = val,
+            FontStyle::Underlined => self.underlined = val,
+        }
+    }
+
+    pub fn stylize_text(&self, text: &str) -> Option<String> {
+        let result = String::new();
+
+        if self.bold {
+            return Some(text.bold());
+        }
+
+        if self.italic {
+            return Some(text.italic());
+        }
+
+        if self.underlined {
+            return Some(text.underlined());
+        }
+
+        None
+    }
+}
